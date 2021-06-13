754b68e533d2bd189c52d093bede3d3d54658901
Maas Lalani <maaslalani0@gmail.com>
2021-06-13 19:05:18 -0400

    feat: execute code on slides

--- internal/model/model.go
+++ internal/model/model.go
@@ -6,6 +6,7 @@ import (
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
+	"github.com/maaslalani/slides/internal/code"
 	"github.com/maaslalani/slides/styles"
 )
 
@@ -16,6 +17,9 @@ type Model struct {
 	Date     string
 	Theme    glamour.TermRendererOption
 	viewport viewport.Model
+	// VirtualText is used for additional information that is not part of the
+	// original slides, it will be displayed on a slide and reset on page change
+	VirtualText string
 }
 
 func (m Model) Init() tea.Cmd {
@@ -37,10 +41,22 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.Page < len(m.Slides)-1 {
 				m.Page++
 			}
+			m.VirtualText = ""
 		case "up", "j", "left", "h", "p":
 			if m.Page > 0 {
 				m.Page--
 			}
+			m.VirtualText = ""
+		case "e":
+			// Run code block
+			block, err := code.Parse(m.Slides[m.Page])
+			if err != nil {
+				// We couldn't parse the code block on the screen
+				m.VirtualText = err.Error()
+				return m, nil
+			}
+			res := code.Execute(block)
+			m.VirtualText = res.Out
 		}
 	}
 	return m, nil
@@ -48,7 +64,9 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	r, _ := glamour.NewTermRenderer(m.Theme, glamour.WithWordWrap(0))
-	slide, err := r.Render(m.Slides[m.Page])
+	slide := m.Slides[m.Page]
+	slide += m.VirtualText
+	slide, err := r.Render(slide)
 	if err != nil {
 		slide = fmt.Sprintf("Error: Could not render markdown! (%v)", err)
 	}
